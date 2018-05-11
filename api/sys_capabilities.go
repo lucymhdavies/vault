a644c02975898e1fd03048969039536166d3aa02
Lucy Davies <lucy.davies@skybettingandgaming.com>
2018-05-11 16:17:47 +0100

    vault token capabilities with multiple paths
    Originally a fix for #4552
    
    With a single path, previous behaviour is preserved:
    
    ```
    $ go run main.go token capabilities secret/foo
    create, delete, list, read, update
    
    $ go run main.go token capabilities -format=json secret/foo
    [
      "create",
      "delete",
      "list",
      "read",
      "update"
    ]
    ```
    
    With multiple paths:
    
    ```
    $ go run main.go token capabilities secret/foo,secret/bar
    Path           Capabilities
    ----           ------------
    secret/foo     create, delete, list, read, update
    secret/bar     create, delete, list, read, update
    
    $ go run main.go token capabilities -format=json secret/foo,secret/bar
    {
      "secret/bar": [
        "create",
        "delete",
        "list",
        "read",
        "update"
      ],
      "secret/foo": [
        "create",
        "delete",
        "list",
        "read",
        "update"
      ]
    }
    ```
    
    This matches what we get when using the `sys/capabilities(-self)` endpoints directly
    
    ```
    $ vault write sys/capabilities-self path=secret/foo
    Key             Value
    ---             -----
    capabilities    [create delete list read update]
    secret/foo      [create delete list read update]
    
    $ vault write sys/capabilities-self path=secret/foo,secret/bar
    Key           Value
    ---           -----
    secret/bar    [create delete list read update]
    secret/foo    [create delete list read update]
    ```
    
    For compatibility, the existing Sys Capabilities(Self) methods remain unchanged.

--- api/sys_capabilities.go
+++ api/sys_capabilities.go
@@ -2,10 +2,19 @@ package api
 
 import "fmt"
 
+// CapabilitiesSelf returns the capabilities of the client token against a given path
+// For compatibility, this is only valid when specifying a single path
 func (c *Sys) CapabilitiesSelf(path string) ([]string, error) {
 	return c.Capabilities(c.c.Token(), path)
 }
 
+// CapabilitiesSelfMultiple returns the capabilities of the client token against given paths
+func (c *Sys) CapabilitiesSelfMultiple(path string) (map[string][]string, error) {
+	return c.CapabilitiesMultiple(c.c.Token(), path)
+}
+
+// Capabilities returns the capabilities of a specified token against a given path
+// For compatibility, this is only valid when specifying a single path
 func (c *Sys) Capabilities(token, path string) ([]string, error) {
 	body := map[string]string{
 		"token": token,
@@ -45,5 +54,51 @@ func (c *Sys) Capabilities(token, path string) ([]string, error) {
 	for _, capability := range capabilitiesRaw {
 		capabilities = append(capabilities, capability.(string))
 	}
+	return capabilities, nil
+}
+
+// CapabilitiesMultiple returns the capabilities of a specified token against given paths
+func (c *Sys) CapabilitiesMultiple(token, path string) (map[string][]string, error) {
+	body := map[string]string{
+		"token": token,
+		"path":  path,
+	}
+
+	reqPath := "/v1/sys/capabilities"
+	if token == c.c.Token() {
+		reqPath = fmt.Sprintf("%s-self", reqPath)
+	}
+
+	r := c.c.NewRequest("POST", reqPath)
+	if err := r.SetJSONBody(body); err != nil {
+		return nil, err
+	}
+
+	resp, err := c.c.RawRequest(r)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result map[string]interface{}
+	err = resp.DecodeJSON(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	data := result["data"].(map[string]interface{})
+
+	capabilities := make(map[string][]string)
+
+	for path, pathCapabilities := range data {
+		capabilities[path] = []string{}
+
+		pathCapabilitiesRaw := pathCapabilities.([]interface{})
+
+		for _, capability := range pathCapabilitiesRaw {
+			capabilities[path] = append(capabilities[path], capability.(string))
+		}
+	}
+
 	return capabilities, nil
 }
